0996552e0af3efc976d92da7a1e64309fcf2c863
Federico Urbani <urbanifederico5@gmail.com>
2023-02-02 18:24:24 +0100

    updated 0202

---
+++ Tester.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Tester {
+
+    public static void main(String[] args) {
+        Random a = new Random();
+        int number = a.nextInt(1, 10);
+        Scanner scan = new Scanner(System.in);
+
+        boolean active = true;
+        System.out.println("Try to guess the num from 1 to 10! give it a try!");
+        while (active) {
+            System.out.println();
+            int guess = scan.nextInt();
+            System.out.println("oh u entered: " + guess + "... good luck!");
+            if (number == guess) {
+                System.out.println("Omg u kinda good aren't u??? the number was "+number+" gj!");
+                active = false;
+            } else {
+                System.out.println("oh unlucky! try again!");
+            }
+        }
+    }
+}
